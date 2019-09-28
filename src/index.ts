c25fce7f7398e980987497a45bcc4f314ca1ef60
Bobby Johnson <Bobby@MadnessLabs.net>
2019-09-28 13:06:10 -0500

    feat(): Auto-load models and schemas

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import * as admin from "firebase-admin";
 import { Initialize } from "fireorm";
 import { ApolloServer, ApolloError, ValidationError, gql } from "apollo-server";
-
+import * as glob from "glob";
 const serviceAccount = require("../service-account.json");
 
 admin.initializeApp({
@@ -11,109 +11,116 @@ admin.initializeApp({
 const firestore = admin.firestore();
 Initialize(firestore);
 
-import { JobModel } from "./models/Job";
-import { UserModel } from "./models/User";
+const models: any = {};
+let modelTypes = ``;
 
-const jobModel = new JobModel();
-const userModel = new UserModel();
-
-const typeDefs = gql`
-  ${userModel.gql}
-
-  ${jobModel.gql}
-
-  type Query {
-    jobs: [Job]
-    job(id: String!): Job
-    users: [User]
-    user(id: String!): User
-  }
-`;
-
-const resolvers = {
-  Query: {
-    async jobs() {
-      try {
-        return (
-          (await jobModel
-            .ref()
-            .limit(15)
-            .get()).docs.map(doc => ({ ...doc.data(), id: doc.id })) ||
-          new ValidationError("Jobs not found")
-        );
-      } catch (error) {
-        throw new ApolloError(error);
-      }
-    },
-    async job(_: null, args: { id: string }) {
-      try {
-        return (
-          (await jobModel.find(args.id)) ||
-          new ValidationError("Job with matching id not found")
-        );
-      } catch (error) {
-        throw new ApolloError(error);
-      }
-    },
-    async users() {
-      try {
-        return (
-          (await userModel
-            .ref()
-            .limit(15)
-            .get()).docs.map(doc => ({ ...doc.data(), id: doc.id })) ||
-          new ValidationError("Users not found")
-        );
-      } catch (error) {
-        throw new ApolloError(error);
-      }
-    },
-    async user(_: null, args: { id: string }) {
-      try {
-        return (
-          (await userModel.find(args.id)) ||
-          new ValidationError("User with matching id not found")
-        );
-      } catch (error) {
-        throw new ApolloError(error);
-      }
-    }
-  },
-  User: {
-    async jobs(user) {
-      try {
-        return (
-          (await userModel.jobsForId(jobModel, user.id)) ||
-          new ValidationError("Jobs not found for user")
-        );
-      } catch (error) {
-        throw new ApolloError(error);
-      }
-    }
-  },
-  Job: {
-    async user(job) {
-      try {
-        return (
-          (await userModel.find(job.user.id)) ||
-          new ValidationError("User for Job not found")
-        );
-      } catch (error) {
-        throw new ApolloError(error);
-      }
+glob("src/models/**/!(Model).ts", async (error, files) => {
+  for (const file of files) {
+    const importedFile = await import(
+      file.replace("src/models", "./models").replace(".ts", "")
+    );
+    const importedModules = Object.keys(importedFile);
+    models[importedModules[0]] = new importedFile[importedModules[1]]();
+    if (models[importedModules[0]].gql) {
+      modelTypes += `${models[importedModules[0]].gql}
+  `;
     }
   }
-};
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  engine: {
-    apiKey: "service:MadnessLabs-9317:L-opKPzu2tawg5jcRTbEyg"
-  },
-  introspection: true
-});
+  const typeDefs = gql`
+    ${modelTypes}
 
-server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+    type Query {
+      jobs: [Job]
+      job(id: String!): Job
+      users: [User]
+      user(id: String!): User
+    }
+  `;
+
+  const resolvers = {
+    Query: {
+      async jobs() {
+        try {
+          return (
+            (await models.Job.ref()
+              .limit(15)
+              .get()).docs.map(doc => ({ ...doc.data(), id: doc.id })) ||
+            new ValidationError("Jobs not found")
+          );
+        } catch (error) {
+          throw new ApolloError(error);
+        }
+      },
+      async job(_: null, args: { id: string }) {
+        try {
+          return (
+            (await models.Job.find(args.id)) ||
+            new ValidationError("Job with matching id not found")
+          );
+        } catch (error) {
+          throw new ApolloError(error);
+        }
+      },
+      async users() {
+        try {
+          return (
+            (await models.User.ref()
+              .limit(15)
+              .get()).docs.map(doc => ({ ...doc.data(), id: doc.id })) ||
+            new ValidationError("Users not found")
+          );
+        } catch (error) {
+          throw new ApolloError(error);
+        }
+      },
+      async user(_: null, args: { id: string }) {
+        try {
+          return (
+            (await models.User.find(args.id)) ||
+            new ValidationError("User with matching id not found")
+          );
+        } catch (error) {
+          throw new ApolloError(error);
+        }
+      }
+    },
+    User: {
+      async jobs(user) {
+        try {
+          return (
+            (await models.User.jobsForId(models.Job, user.id)) ||
+            new ValidationError("Jobs not found for user")
+          );
+        } catch (error) {
+          throw new ApolloError(error);
+        }
+      }
+    },
+    Job: {
+      async user(job) {
+        try {
+          return (
+            (await models.User.find(job.user.id)) ||
+            new ValidationError("User for Job not found")
+          );
+        } catch (error) {
+          throw new ApolloError(error);
+        }
+      }
+    }
+  };
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    engine: {
+      apiKey: "service:MadnessLabs-9317:L-opKPzu2tawg5jcRTbEyg"
+    },
+    introspection: true
+  });
+
+  server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
 });
